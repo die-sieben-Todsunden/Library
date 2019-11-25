76f8d4e0a4c382ba423736ab279d479de8f3c200
1753048 <1753048@student.hcmus.edu.vn>
2019-11-25 23:13:25 +0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // set static folder
-<<<<<<< HEAD
-<<<<<<< HEAD
-app.use(express.static(__dirname + '/Home_guest.html'));
-=======
-app.use(express.static(__dirname + '/userHomepage.html'));
->>>>>>> 10b524f45547e5992758c8c86a15198a7f417f29
-=======
-app.use(express.static(__dirname + '/'));
->>>>>>> 122d013c213b4df25fff953b58de0ec6d6eec015
+
+app.use(express.static(__dirname + "/Home_guest.html"));
 
 // start server
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), () => {
-    console.log(`server is listening on port ${app.get('port')}`);
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), () => {
+  console.log(`server is listening on port ${app.get("port")}`);
 });
