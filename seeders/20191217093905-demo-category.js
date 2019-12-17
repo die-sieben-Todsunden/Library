d8b36a0ce863cf4339987a474a2b05869d870e6f
dinh <1753003@student.hcmus.edu.vn>
2019-12-17 16:41:30 +0700

    demo seed category

---
+++ seeders/20191217093905-demo-category.js
@@ -0,0 +1,36 @@
+'use strict';
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('database_sample/demo-book.json');
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    let data = JSON.parse(rawdata);
+    data.map(item =>{
+      item.createdAt = Sequelize.literal('NOW()');
+      item.updatedAt = Sequelize.literal('NOW()');
+      return item;
+    })
+    return queryInterface.bulkInsert('bookInfos', data, {});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('bookInfos', null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
