1f5f33011adda3d20ce73889e1d16c72b3c8ba52
1753048 <1753048@student.hcmus.edu.vn>
2019-12-20 12:12:24 +0700

    test

--- seeders/20191219040655-demo-user.js
+++ seeders/20191219040655-demo-user.js
@@ -1,17 +1,30 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     let data = [
       {
-        userName: 'dinh',
-        password: '$2a$10$.awhk3N2XApgWrq/R2dexej1XM3737nnQ/yM.S56KiY4R7iMVW2hG',// 123
-        name: 'Dinh nguyen',
-        personalID: '1753003',
-        address: 'Binh Thanh, TP Ho Chi Minh',
-        birth:'1999-05-05',
-        phone: '112423412',
-        email:'1753003@student.hcmus.edu.vn'    
+        userName: "dinh",
+        password:
+          "$2a$10$.awhk3N2XApgWrq/R2dexej1XM3737nnQ/yM.S56KiY4R7iMVW2hG", // 123
+        name: "Dinh nguyen",
+        personalID: "1753003",
+        address: "Binh Thanh, TP Ho Chi Minh",
+        birth: "1999-05-05",
+        phone: "112423412",
+        email: "1753003@student.hcmus.edu.vn"
+      },
+      {
+        userName: "admin",
+        password:
+          "$2a$10$.awhk3N2XApgWrq/R2dexej1XM3737nnQ/yM.S56KiY4R7iMVW2hG", // 123
+        name: "Admin",
+        personalID: "12312313",
+        address: "Binh Thanh, TP Ho Chi Minh",
+        birth: "1999-05-05",
+        phone: "112423412",
+        email: "admin@gmail.com",
+        role: "admin"
       }
     ];
     data.map(item => {
