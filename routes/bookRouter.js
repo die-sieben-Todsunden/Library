e996a3ca4dea1bf915a3cb2cbf8813679381ef6b
1753057 <1753057@student.hcmus.edu.vn>
2019-12-20 19:59:26 +0700

    add view Detail and view List

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,29 +1,52 @@
 let express = require("express");
 let router = express.Router();
 
-router.get("/", (req, res, next)=> {
-  if(req.query.sort== null){
-    req.query.sort = 'null';
-  }
-  // if(req.query.limit == null ||isNaN(req.query.limit)){
-  //   req.query.limit = 9;
-  // }
-  if(req.query.page == null ||isNaN(req.query.page)){
-    req.query.page = 9;
-  }
-  if(req.query.search== null || (req.query.search.trim() == '')){
-    req.query.search = '';
-  }
-  let bookController = require('../controllers/bookController');
-  console.log(req.query.search);
-  if()
-  bookController
-    .getAll(req.query)
-    .then(data=>{
-      console.log(data.length);
-      res.locals.books = data;
-      res.render('lookup');
-    })
+router.get("/", (req, res, next) => {
+    if (req.query.sort == null) {
+        req.query.sort = 'null';
+    }
+    // if(req.query.limit == null ||isNaN(req.query.limit)){
+    //   req.query.limit = 9;
+    // }
+    if (req.query.page == null || isNaN(req.query.page)) {
+        req.query.page = 9;
+    }
+    if (req.query.search == null || (req.query.search.trim() == '')) {
+        req.query.search = '';
+    }
+    let bookController = require('../controllers/bookController');
+    console.log(req.query.search);
+    bookController
+        .getAll(req.query)
+        .then(data => {
+            console.log(data.length);
+            res.locals.books = data;
+            res.render('lookup');
+        })
+        .catch(error => {
+            next(error);
+        })
 });
+
+
+router.get("/:id", (req, res, next) => {
+    let bookController = require('../controllers/bookController');
+    bookController
+        .getById(req.params.id)
+        .then(data => {
+            res.locals.book = data;
+            return bookController.getAllIndex();
+
+        })
+        .then(result => {
+            res.locals.same = result;
+            res.render('book_info');
+        })
+        .catch(error => {
+            next(error);
+        })
+});
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,22 @@ let express = require("express");
 let router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+    let bookController = require('../controllers/bookController');
+    bookController
+        .getAllIndex()
+        .then(data => {
+            res.locals.books = data;
+            res.render('index');
+        })
 });
 router.get("/about", (req, res) => {
-  res.render("about");
+    res.render("about");
 });
 router.get("/lookup", function(req, res) {
-  res.render("lookup");
+    res.render("lookup");
 });
 router.get("/lookup_res1", function(req, res) {
-  res.render("lookup_res1");
+    res.render("lookup_res1");
 });
 
 module.exports = router;
