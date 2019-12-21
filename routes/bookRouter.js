5ec1e3703673c38b76d6e33e7c93cad9f8f96446
1753048 <1753048@student.hcmus.edu.vn>
2019-12-21 11:05:54 +0700

    fix

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,29 +1,27 @@
 let express = require("express");
 let router = express.Router();
 
-router.get("/", (req, res, next)=> {
-  if(req.query.sort== null){
-    req.query.sort = 'null';
+router.get("/", (req, res, next) => {
+  if (req.query.sort == null) {
+    req.query.sort = "null";
   }
   // if(req.query.limit == null ||isNaN(req.query.limit)){
   //   req.query.limit = 9;
   // }
-  if(req.query.page == null ||isNaN(req.query.page)){
+  if (req.query.page == null || isNaN(req.query.page)) {
     req.query.page = 9;
   }
-  if(req.query.search== null || (req.query.search.trim() == '')){
-    req.query.search = '';
+  if (req.query.search == null || req.query.search.trim() == "") {
+    req.query.search = "";
   }
-  let bookController = require('../controllers/bookController');
+  let bookController = require("../controllers/bookController");
   console.log(req.query.search);
-  if()
-  bookController
-    .getAll(req.query)
-    .then(data=>{
-      console.log(data.length);
-      res.locals.books = data;
-      res.render('lookup');
-    })
+
+  bookController.getAll(req.query).then(data => {
+    console.log(data.length);
+    res.locals.books = data;
+    res.render("lookup");
+  });
 });
 
 module.exports = router;
